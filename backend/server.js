b36c0feb8323fa5e500583c9c9dbfaab13f50ec2
Sibiraj s <s.sibiraj2004@gmail.com>
2026-01-29 11:46:08 +0530

    Update server.js
    updated

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const errorHandler = require('./middleware/errorHandler');
+const errorHandler = require('./middleware/ErrorHandler');
 
 // Load env vars
 dotenv.config();
